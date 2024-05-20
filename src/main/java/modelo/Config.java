8e8b0c1d13898f24bcb6c6a8bcdf2271206d2874
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-05-20 13:01:59 +0200

    Primera config

---
+++ src/main/java/modelo/Config.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public interface Config {
+
+	public static final String HOST = "localhost";
+	public static final String USERNAME = "";
+	public static final String BBDD = "destino_cabalero";
+	public static final String PASSWORD = "";
+
+}
